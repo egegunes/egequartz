f9f26cb339ddec0b52c25612440a8894697d4f25
Ege Günes <egegunes@gmail.com>
2026-08-16 11:28:15 +0300

    add fathom

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "fathom",
+      siteId: "HGORAWFR",
     },
     locale: "en-US",
     baseUrl: "ege.dev",
