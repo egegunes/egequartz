6039a3985d5bc5262a3d126d505bd31c33987f27
Ege Günes <egegunes@gmail.com>
2026-08-21 23:10:01 +0300

    fix mobile

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import { SimpleSlug } from "./quartz/util/path"
 const recentNotes = [
   Component.RecentNotes({
     title: "Recent Posts",
-    limit: 4,
+    limit: 5,
     filter: (f) => f.slug!.startsWith("posts/") && f.slug! !== "posts/index",
     linkToMore: "posts/" as SimpleSlug,
   }),
